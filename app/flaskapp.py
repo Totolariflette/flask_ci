ebeed33de93bf7b6f84df96ac0d3bb82a97e82b9
Totolariflette <62987676+Totolariflette@users.noreply.github.com>
2020-11-24 15:59:18 +0100

    all files added

---
+++ app/flaskapp.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from redis import Redis, RedisError
+
+redis = Redis(host="redis-server", db=0)
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+	return "<h1> Hello my biatchou zouzou !! "
+
+@app.route('/visit')
+def counter_incr():
+	try:
+		visits = redis.incr("counter")
+	except:
+		visits = "<i>cannot connect to redis servers</i>"
+    html = "<h1>Number of visits : {}</h1>".format(visits)
+    return html
+
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=80)
